4ebb1a4ada0945fa8e5bc1e9632c99d0f3074a54
Ol4ik88 <slapikol4ik@gmail.com>
2022-05-08 22:29:14 +0300

    feat: add arrays with languages

---
+++ js/languages/en.js
@@ -0,0 +1,322 @@
+export default [
+  {
+    normal: '`',
+    shift: '~',
+    code: 'Backquote',
+  },
+  {
+    normal: '1',
+    shift: '!',
+    code: 'Digit1',
+  },
+  {
+    normal: '2',
+    shift: '@',
+    code: 'Digit2',
+  },
+  {
+    normal: '3',
+    shift: '#',
+    code: 'Digit3',
+  },
+  {
+    normal: '4',
+    shift: '$',
+    code: 'Digit4',
+  },
+  {
+    normal: '5',
+    shift: '%',
+    code: 'Digit5',
+  },
+  {
+    normal: '6',
+    shift: '^',
+    code: 'Digit6',
+  },
+  {
+    normal: '7',
+    shift: '&',
+    code: 'Digit7',
+  },
+  {
+    normal: '8',
+    shift: '*',
+    code: 'Digit8',
+  },
+  {
+    normal: '9',
+    shift: '(',
+    code: 'Digit9',
+  },
+  {
+    normal: '0',
+    shift: ')',
+    code: 'Digit0',
+  },
+  {
+    normal: '-',
+    shift: '_',
+    code: 'Minus',
+  },
+  {
+    normal: '=',
+    shift: '+',
+    code: 'Equal',
+  },
+  {
+    normal: 'Backspace',
+    shift: null,
+    code: 'Backspace',
+  },
+  {
+    normal: 'Tab',
+    shift: null,
+    code: 'Tab',
+  },
+  {
+    normal: 'q',
+    shift: 'Q',
+    code: 'KeyQ',
+  },
+  {
+    normal: 'w',
+    shift: 'W',
+    code: 'KeyW',
+  },
+  {
+    normal: 'e',
+    shift: 'E',
+    code: 'KeyE',
+  },
+  {
+    normal: 'r',
+    shift: 'R',
+    code: 'KeyR',
+  },
+  {
+    normal: 't',
+    shift: 'T',
+    code: 'KeyT',
+  },
+  {
+    normal: 'y',
+    shift: 'Y',
+    code: 'KeyY',
+  },
+  {
+    normal: 'u',
+    shift: 'U',
+    code: 'KeyU',
+  },
+  {
+    normal: 'i',
+    shift: 'I',
+    code: 'KeyI',
+  },
+  {
+    normal: 'o',
+    shift: 'O',
+    code: 'KeyO',
+  },
+  {
+    normal: 'p',
+    shift: 'P',
+    code: 'KeyP',
+  },
+  {
+    normal: '[',
+    shift: '{',
+    code: 'BracketLeft',
+  },
+  {
+    normal: ']',
+    shift: '}',
+    code: 'BracketRight',
+  },
+  {
+    normal: '\\',
+    shift: '|',
+    code: 'Backslash',
+  },
+  {
+    normal: 'Del',
+    shift: null,
+    code: 'Delete',
+  },
+  {
+    normal: 'CapsLock',
+    shift: null,
+    code: 'CapsLock',
+  },
+  {
+    normal: 'a',
+    shift: 'A',
+    code: 'KeyA',
+  },
+  {
+    normal: 's',
+    shift: 'S',
+    code: 'KeyS',
+  },
+  {
+    normal: 'd',
+    shift: 'D',
+    code: 'KeyD',
+  },
+  {
+    normal: 'f',
+    shift: 'F',
+    code: 'KeyF',
+  },
+  {
+    normal: 'g',
+    shift: 'G',
+    code: 'KeyG',
+  },
+  {
+    normal: 'h',
+    shift: 'H',
+    code: 'KeyH',
+  },
+  {
+    normal: 'j',
+    shift: 'J',
+    code: 'KeyJ',
+  },
+  {
+    normal: 'k',
+    shift: 'K',
+    code: 'KeyK',
+  },
+  {
+    normal: 'l',
+    shift: 'L',
+    code: 'KeyL',
+  },
+  {
+    normal: ';',
+    shift: ':',
+    code: 'Semicolon',
+  },
+  {
+    normal: '&acute;',
+    shift: '"',
+    code: 'Quote',
+  },
+  {
+    normal: 'Enter',
+    shift: null,
+    code: 'Enter',
+  },
+  {
+    normal: 'Shift',
+    shift: null,
+    code: 'ShiftLeft',
+  },
+  {
+    normal: 'z',
+    shift: 'Z',
+    code: 'KeyZ',
+  },
+  {
+    normal: 'x',
+    shift: 'X',
+    code: 'KeyX',
+  },
+  {
+    normal: 'c',
+    shift: 'C',
+    code: 'KeyC',
+  },
+  {
+    normal: 'v',
+    shift: 'V',
+    code: 'KeyV',
+  },
+  {
+    normal: 'b',
+    shift: 'B',
+    code: 'KeyB',
+  },
+  {
+    normal: 'n',
+    shift: 'N',
+    code: 'KeyN',
+  },
+  {
+    normal: 'm',
+    shift: 'M',
+    code: 'KeyM',
+  },
+  {
+    normal: ',',
+    shift: '<',
+    code: 'Comma',
+  },
+  {
+    normal: '.',
+    shift: '>',
+    code: 'Period',
+  },
+  {
+    normal: '/',
+    shift: '?',
+    code: 'Slash',
+  },
+  {
+    normal: '&uarr;',
+    shift: null,
+    code: 'ArrowUp',
+  },
+  {
+    normal: 'Shift',
+    shift: null,
+    code: 'ShiftRight',
+  },
+  {
+    normal: 'Ctrl',
+    shift: null,
+    code: 'ControlLeft',
+  },
+  {
+    normal: 'Win',
+    shift: null,
+    code: 'Win',
+  },
+  {
+    normal: 'Alt',
+    shift: null,
+    code: 'AltLeft',
+  },
+  {
+    normal: '',
+    shift: null,
+    code: 'Space',
+  },
+  {
+    normal: 'Alt',
+    shift: null,
+    code: 'AltRight',
+  },
+  {
+    normal: '&larr;',
+    shift: null,
+    code: 'ArrowLeft',
+  },
+  {
+    normal: '&darr;',
+    shift: null,
+    code: 'ArrowDown',
+  },
+  {
+    normal: '&rarr;',
+    shift: null,
+    code: 'ArrowRight',
+  },
+  {
+    normal: 'Ctrl',
+    shift: null,
+    code: 'ControlRight',
+  },
+];
