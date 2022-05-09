fed7b0cf4416d4d2e6434e5dcbe1360d75837d7d
Ol4ik88 <slapikol4ik@gmail.com>
2022-05-10 00:43:22 +0300

    fix: implement correct constructor

--- js/keys.js
+++ js/keys.js
@@ -5,11 +5,11 @@ export default class Key {
     this.normal = normal;
     this.shift = shift;
     this.code = code;
-    if (normal.match(/Tab|Caps|Shift|Ctrl|Win|Alt|Backspace|Del|Enter|arr/)) {
+    if (normal.match(/Tab|Caps|Shift|Ctrl|Alt|Backspace|Del|Enter|arr/)) {
       this.isFunctional = true;
     } else this.isFunctional = false;
 
-    if (shift && shift.match(/[^0-9A-Za-zА-Яа-я]/)) {
+    if (shift && shift.match(/[^0-9A-Za-zА-Яа-яёЁ]/)) {
       this.sign = create('div', 'sign', shift);
     } else {
       this.sign = create('div', 'sign', '');
